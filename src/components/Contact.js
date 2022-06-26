c79ca2665ff8532b7fef67fcdac71769947cc283
regycloud <regycloud@yahoo.com>
2022-06-26 22:46:13 +0700

    task 15

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './contact.css';
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
+        <>
+            <div className='container'>
+                <div className='card'>
+                    <img className='img' src={data.photo} alt={data.name}/>
+
+                    <div className='content'>
+                    <h3>{data.name}</h3>
+                    <p>{data.name}</p>
+                    <p>{data.email}</p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './header.css'
 
 const Header = () => {
     return (
-        <></>
+        <>
+        <h1>Call a Friend</h1>
+        <p>your friendly contact app</p>
+        <p className='star'>*********************************</p>
+        </>
     )
 }
 
